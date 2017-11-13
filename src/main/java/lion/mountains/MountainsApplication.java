19d675b1a004c9a4ffd6f256855d3e51b58bd069
葛尧 <geyao2012@gmail.com>
2017-11-13 21:06:36 +0800

    Initial commit

---
+++ src/main/java/lion/mountains/MountainsApplication.java
@@ -0,0 +1,12 @@
+package lion.mountains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainsApplication.class, args);
+	}
+}
